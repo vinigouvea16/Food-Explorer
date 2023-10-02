a3c9364e48d9c1e942ec21843f5cc0a8fa1eaa9d
Vinicius Gouvea <vrgouvea@outlook.com>
2023-10-02 20:16:53 -0300

    HEAD method added

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export const api = axios.create({
   headers: {
     'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT,HEAD',
     'Access-Control-Allow-Headers':
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
   },
