1f60a50f09c6731ea50ed2fdfdfa3665f0b0a4b8
Vinicius Gouvea <vrgouvea@outlook.com>
2023-10-03 15:28:02 -0300

    img test

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Container, Content } from './style'
 // import {Button} from '../../components/Button'
 import { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import homeimg from '../../../assets/homeimg.png'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { MyCarousel } from '../../components/MyCarousel'
@@ -9,7 +10,6 @@ import { Section } from '../../components/Section'
 import { SideMenu } from '../../components/SideMenu'
 import { useDebounce } from '../../hooks/useDebounce'
 import { api } from '../../services/api'
-
 function Home() {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
   const [data, setData] = useState([])
@@ -20,6 +20,7 @@ function Home() {
   const [meals, setMeals] = useState([])
   const [desserts, setDesserts] = useState([])
   const [drinks, setDrinks] = useState([])
+
   function categoryFilter(data) {
     const meals = data.filter((dish) => dish.category === 'Refeições')
     const desserts = data.filter((dish) => dish.category === 'Sobremesas')
@@ -66,7 +67,7 @@ function Home() {
       />
       <Content>
         <div className="mainImg">
-          <img src="/assets/homeimg.png" alt="" />
+          <img src={homeimg} alt="" />
           <hgroup>
             <h2>Sabores Inigualáveis</h2>
             <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
