cf01f024234e586309da9efb5bfcbaf9e789123e
Vinicius Gouvea <vrgouvea@outlook.com>
2023-10-03 14:42:55 -0300

    native fetch test

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -12,7 +12,6 @@ function AuthProvider({ children }) {
           'Content-type': 'application/json',
           Accept: 'application/json',
         },
-        credentials: 'include',
         body: JSON.stringify({ email, password }),
       })
       if (!response.ok) {
